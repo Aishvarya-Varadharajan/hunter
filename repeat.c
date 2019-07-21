38856e43636d0135312c34794d37204edf5b6d44
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-07-21 22:19:33 +0530

    Update repeat.c

--- repeat.c
+++ repeat.c
@@ -1,44 +1,65 @@
 #include<stdio.h>
 int main()
 {
-  int num,i,j,k=0,l;
-  scanf("%d",&num);
-  int arr[num],arr1[num];
-  for(i=0;i<num;i++)
-  {
-    scanf("%d",&arr[i]);
-  }
-  for(i=0;i<num;i++)
-  {
-    for(j=i+1;j<num;j++)
-    {
-      if(arr[i] == arr[j])
-      {
-        for(l=0;l<k;l++)
-        {
-          if(arr1[k] != arr[i])
-          {
-             arr1[k] = arr[i];
-           }
-           k++;
-         }
-       }  
-    }
-  }
-  for(i=0;i<k-1;i++)
-  {
-    for(j=0;j<k-1;j++)
-    {
-      if(arr1[i] == arr1[j])
-      {
-        temp=arr1[i];
-        arr1[i]=arr1[j];
-        arr1[j] = temp;
-      }
-  }
-  for(i=0;i<k-1;i++)
-  {
-    printf("%d",arr1[i]);
-  }
-  return 0;
+	int num,i,j,k=0,temp;
+	scanf("%d",&num);
+	if((num >= 1) && (num <= 100000))
+	{
+		int a[num],arr[num],arr1[num];
+		for(i=0;i<num;i++)
+		{
+			scanf("%d",&a[i]);
+		}
+		for(i=0;i<num;i++)
+		{
+			for(j=i+1;j<num;j++)
+			{
+				if(a[i] == a[j])
+				{
+					arr[k] = a[i];
+					k++;
+
+				}
+			}
+		}
+//	k=k-1;
+		for(i=0;i<k;i++)
+		{
+			for(j=i+1;j<k;j++)
+			{
+				if(arr[i] == arr[j])
+				{
+					arr[j] = 0;
+					k--;
+				}
+			}
+		}
+		j=0;
+		for(i=0;i<k;i++)
+		{
+			if(arr[i] != 0)
+			{
+				arr1[j]=arr[i];
+				j++;
+			}
+		}
+		k=j;
+		for(i=0;i<k;i++)
+		{
+			for(j=i+1;j<k;j++)
+			{
+				if(arr1[i] > arr1[j])
+				{
+					temp=arr[j];
+					arr[j]=arr[i];
+					arr[i]=temp;
+				}
+			}
+		}
+		for(i=0;i<k;i++)
+		{
+			printf("%d ",arr[i]);
+		}
+	}
+		return 0;
 }
